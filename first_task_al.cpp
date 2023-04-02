da528cf5437a628740f7a21465adb16bab0ebfa2
jalileus <alkhazhali.zh@phystech.edu>
2023-04-02 20:11:45 +0300

    Create first_task_al.cpp

---
+++ first_task_al.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main()
+{
+  unsigned long long int Number_;
+  unsigned long long int rev = 0;
+
+  std::cin>>Number_;
+
+  while(Number_ != 0)
+  {
+  rev = (rev * 10) + (Number_ % 10);
+  Number_ = Number_/10;
+  }
+
+  std::cout<<rev;
+
+  return 0;
+}
