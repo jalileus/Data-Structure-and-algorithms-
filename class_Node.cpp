2c510810a7c26f6b1c7634be1aac30e5f42a2efa
jalileus <128904203+jalileus@users.noreply.github.com>
2023-04-07 16:49:34 +0300

    Update class_Node.cpp
    https://contest.yandex.ru/contest/47619/run-report/85314077/

--- class_Node.cpp
+++ class_Node.cpp
@@ -1,82 +1,63 @@
-#include<iostream>
+#include <iostream>
 
-struct Node
-{
-    public:
-    char item_;
-    struct Node* next;
-    Node(char item_)
-    {
-        this->item_ = item_;
-        next = NULL;
-    }
+struct Node {
+  char item;
+  struct Node *next;
+  Node(char item) {
+    this->item = item;
+    next = 0;
+  }
 };
 
-class linked_list
-{  
-    Node* head;
+class linked_list {
+  Node *head;
+
 public:
-    linked_list(){head = NULL;}
+  linked_list(std::string data) {
+    head = NULL;
+    for (int i = 0; i < data.length(); i++) {
+      Node *New = new Node(data[i]);
 
-    void Reversed()
-    {
-        Node *curr = head;
-        Node *next = NULL;
-        Node *prev = NULL;
-
-        while(curr != NULL)
-        {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        head = prev;
+      if (head == 0) {
+        head = New;
+      } else {
+        Node *last = head;
+        while (last->next != 0)
+          last = last->next;
+        last->next = New;
+      }
     }
+  }
 
-    void Insert(char item_1)
-    {
-        Node* New = new Node(item_1);
- 
-        if(head == NULL)
-        {
-            head = New;
-            return;
-        }
-        else
-        {
-            Node* last = head;
-            while(last->next != NULL)
-                last = last->next;
-            last->next = New;
-            return;
-        }
-    }
-    
-    void Print()
-    {
-        struct Node* New = head;
-        while(New != NULL)
-        {
-            std::cout<<New->item_;
-            New = New->next;
-        }
-    }
+  ~linked_list() {}
 
+  void Reversed() {
+    Node *current = head;
+    Node *next = 0;
+    Node *previous = 0;
+
+    while (current != 0) {
+      next = current->next;
+      current->next = previous;
+      previous = current;
+      current = next;
+    }
+    head = previous;
+  }
+  void Print() {
+    struct Node *New = head;
+    while (New != 0) {
+      std::cout << New->item;
+      New = New->next;
+    }
+  }
 };
+int main() {
+  std::string data;
+  std::getline(std::cin, data);
+  linked_list list(data);
+  list.Reversed();
+  list.Print();
 
-int main()
-{
-    linked_list l;
-    std::string s;
-    std::getline(std::cin,s);
-    for(int i=0; i<s.length(); i++)
-    {
-        l.Insert(s[i]);
-    }
-
-    l.Reversed();
-    l.Print();
-
-    return 0;
+  return 0;
 }
