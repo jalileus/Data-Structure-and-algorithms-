617fc7a01c341576ac4d95900fcbdfc8aadc852f
jalileus <128904203+jalileus@users.noreply.github.com>
2023-04-07 13:57:23 +0300

    Update first_task_al.cpp

--- first_task_al.cpp
+++ first_task_al.cpp
@@ -1,19 +1,17 @@
-#include<iostream>
+#include <iostream>
 
-int main()
-{
+int main() {
   unsigned long long int Number_;
   unsigned long long int rev = 0;
 
-  std::cin>>Number_;
+  std::cin >> Number_;
 
-  while(Number_ != 0)
-  {
-  rev = (rev * 10) + (Number_ % 10);
-  Number_ = Number_/10;
+  for (; Number_ != 0;) {
+    rev = (rev * 10) + (Number_ % 10);
+    Number_ = Number_ / 10;
   }
 
-  std::cout<<rev;
+  std::cout << rev;
 
   return 0;
 }
