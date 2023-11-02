281db65ee1dbacabaa85e398208eabe185576e26
jalileus <128904203+jalileus@users.noreply.github.com>
2023-11-02 20:41:19 +0300

    Update grass_shoper.cpp

--- grass_shoper.cpp
+++ grass_shoper.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
 
-unsigned long long T[5] = {8, 4, 2, 1, 1};
-unsigned long long F[5][5] = {{1, 1, 1, 1, 1},
-                              {1, 0, 0, 0, 0},
-                              {0, 1, 0, 0, 0},
-                              {0, 0, 1, 0, 0},
-                              {0, 0, 0, 1, 0}};
-unsigned long long M[5][5] = {{1, 1, 1, 1, 1},
-                              {
-                                  1,
-                                  0,
-                                  0,
-                                  0,
-                                  0,
-                              },
-                              {0, 1, 0, 0, 0},
-                              {0, 0, 1, 0, 0},
-                              {0, 0, 0, 1, 0}};
+uint64_t T[5] = {8, 4, 2, 1, 1};
+uint64_t F[5][5] = {{1, 1, 1, 1, 1},
+                    {1, 0, 0, 0, 0},
+                    {0, 1, 0, 0, 0},
+                    {0, 0, 1, 0, 0},
+                    {0, 0, 0, 1, 0}};
+uint64_t M[5][5] = {{1, 1, 1, 1, 1},
+                    {
+                        1,
+                        0,
+                        0,
+                        0,
+                        0,
+                    },
+                    {0, 1, 0, 0, 0},
+                    {0, 0, 1, 0, 0},
+                    {0, 0, 0, 1, 0}};
 
-void MultiplyMatrix(unsigned long long M_1[5][5],
-                    unsigned long long M_2[5][5]) {
-  unsigned long long temp[5][5] = {{1, 1, 1, 1, 1},
-                                   {
-                                       1,
-                                       0,
-                                       0,
-                                       0,
-                                       0,
-                                   },
-                                   {0, 1, 0, 0, 0},
-                                   {0, 0, 1, 0, 0},
-                                   {0, 0, 0, 1, 0}};
+void MultiplyMatrix(uint64_t M_1[5][5], uint64_t M_2[5][5]) {
+  uint64_t temp[5][5] = {{1, 1, 1, 1, 1},
+                         {
+                             1,
+                             0,
+                             0,
+                             0,
+                             0,
+                         },
+                         {0, 1, 0, 0, 0},
+                         {0, 0, 1, 0, 0},
+                         {0, 0, 0, 1, 0}};
   for (int i = 0; i < 5; i++) {
     for (int j = 0; j < 5; j++) {
-      unsigned long long sum = 0;
+      uint64_t sum = 0;
       for (int k = 0; k < 5; k++) {
         sum = (sum + ((M_1[i][k] % 1000003) * (M_2[k][j] % 1000003))) % 1000003;
       }
@@ -47,11 +46,10 @@ void MultiplyMatrix(unsigned long long M_1[5][5],
   }
 }
 
-unsigned long long LastMultiplication(unsigned long long M_3[5][5],
-                                      unsigned long long T[5]) {
-  unsigned long long t[5] = {0, 0, 0, 0, 0};
+uint64_t LastMultiplication(uint64_t M_3[5][5], uint64_t T[5]) {
+  uint64_t t[5] = {0, 0, 0, 0, 0};
   for (int i = 0; i < 5; i++) {
-    unsigned long long sum = 0;
+    uint64_t sum = 0;
     for (int k = 0; k < 5; k++) {
       sum = (sum + ((M_3[i][k] % 1000003) * (T[k] % 1000003))) % 1000003;
     }
@@ -60,7 +58,7 @@ unsigned long long LastMultiplication(unsigned long long M_3[5][5],
   return t[0];
 }
 
-void PowerMatrix(unsigned long long F[5][5], unsigned long long n) {
+void PowerMatrix(uint64_t F[5][5], uint64_t n) {
   if (n == 0 || n == 1) {
     return;
   }
@@ -71,7 +69,7 @@ void PowerMatrix(unsigned long long F[5][5], unsigned long long n) {
   }
 }
 
-unsigned long long CountSteps(unsigned long long n) {
+uint64_t CountSteps(uint64_t n) {
   if (n == 0) {
     return 0;
   } else if (n == 1 || n == 2) {
@@ -88,7 +86,7 @@ unsigned long long CountSteps(unsigned long long n) {
 }
 
 int main() {
-  unsigned long long n;
+  uint64_t n;
   std::cin >> n;
   std::cout << CountSteps(n) << std::endl;
   return 0;
