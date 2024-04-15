3cf8baacb71a6d6535d94b3485b8db0182a005c3
jalileus <alkhazhali.zh@phystech.edu>
2024-04-16 02:59:03 +0300

    Update guess_game.cpp
    GNU GCC 13.1 C++20 : https://contest.yandex.ru/contest/59940/run-report/112150708/
     (Make) Clang 17.0.1 C++20 : https://contest.yandex.ru/contest/59940/run-report/112150716/

--- guess_game.cpp
+++ guess_game.cpp
@@ -3,24 +3,27 @@
 #include <limits>
 #include <vector>
 
-int GetMinCost(int n, std::vector<std::vector<int>> dp) {
-  for (int l = 2; l <= n; l++) {
-    for (int i = 1; i <= n - l + 1; i++) {
-      int min_cost = std::numeric_limits<int>::max();
-      for (int pivot = i + (l - 1) / 2; pivot < i + l - 1; pivot++) {
-        int temp = pivot + std::max(dp[i][pivot - 1], dp[pivot + 1][i + l - 1]);
+std::size_t GetMinCost(std::size_t upper_bound,
+                       std::vector<std::vector<std::size_t>> dp) {
+  for (std::size_t l = 2; l <= upper_bound; l++) {
+    for (std::size_t i = 1; i <= upper_bound - l + 1; i++) {
+      std::size_t min_cost = std::numeric_limits<std::size_t>::max();
+      for (std::size_t pivot = i + (l - 1) / 2; pivot < i + l - 1; pivot++) {
+        std::size_t temp =
+            pivot + std::max(dp[i][pivot - 1], dp[pivot + 1][i + l - 1]);
         min_cost = std::min(temp, min_cost);
       }
       dp[i][i + l - 1] = min_cost;
     }
   }
-  return dp[1][n];
+  return dp[1][upper_bound];
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  std::vector<std::vector<int>> dp(n + 1, std::vector<int>(n + 1, 0));
-  std::cout << GetMinCost(n, dp) << std::endl;
+  std::size_t upper_bound;
+  std::cin >> upper_bound;
+  std::vector<std::vector<std::size_t>> dp(
+      upper_bound + 1, std::vector<std::size_t>(upper_bound + 1, 0));
+  std::cout << GetMinCost(upper_bound, dp) << std::endl;
   return 0;
 }
