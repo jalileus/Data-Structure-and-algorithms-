52fdd5c580a3a598fb831bb5921731b4d9004465
Jalal Alhajali <alhajalijalal@gmail.com>
2024-10-07 18:27:04 +0300

    Update and rename the_rick.cpp to graph_ shortest _path.cpp

--- graph_ shortest _path.cpp
+++ graph_ shortest _path.cpp
@@ -1,3 +1,40 @@
+/*
+Time limit	2.5 seconds
+Memory limit	256Mb
+Input	standard input or input.txt
+Output	standard output or output.txt
+
+problem :
+Rick and Morty are once again roaming the universes, but they decided to limit themselves to only those whose numbers are less than M. 
+They can teleport from universe z to universe (z+1) mod M for a bottles of lemonade or to universe (z2+1) mod M for b bottles of lemonade. 
+Rick and Morty want to get from universe x to universe y. How many bottles of lemonade will Rick give for such a trip if he wants to spend as little as possible?
+
+Input format
+A line in a row contains the number of bottles a for the first type of teleportation, the number of bottles b for the second type of teleportation, 
+the number of universes M, the number of the starting universe x, the number of the final universe y (0 ≤ a, b ≤ 100, 1 ≤ M ≤ 106, 0 ≤ x, y < M).
+
+Output format
+It is required to find a single number — the number of bottles of lemonade that Rick will give for such a trip.
+
+Sample 1
+Input	
+3 14 15 9 9
+Output
+0
+
+Sample 2
+Input	
+6 1 5 2 3
+Output
+6
+
+Sample 3
+Input	
+6 1 5 2 1
+Output
+2
+*/
+
 #include <iostream>
 #include <limits>
 #include <list>
